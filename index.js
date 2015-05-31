56491709a3760208e7cc6e460f1ded0fc8a18c46
William Cotton <williamcotton@gmail.com>
2015-05-31 10:23:04 -0700

    oops

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
