1c9be0765f492c73d22d195ecdd4327adfb802f6
William Cotton <williamcotton@gmail.com>
2018-02-10 15:04:12 -0600

    fix a few small issues and update to heroku local

--- src/components/root-component.jsx
+++ src/components/root-component.jsx
@@ -1,32 +1,40 @@
-const React = require('react')
-const {NavItem} = require('react-bootstrap')
-const {Header, Footer} = require('./decibel')
+const React = require('react');
+const { NavItem } = require('react-bootstrap');
+const { Header, Footer } = require('./decibel');
 
 const RootComponent = React.createClass({
   propTypes: {
     user: React.PropTypes.object,
-    content: React.PropTypes.element
+    content: React.PropTypes.element,
   },
-  render () {
-    const content = this.props.content
-    const navRight = this.props.user
-      ? <NavItem key='/logout' href='/logout'>Logout</NavItem>
-      : <NavItem key='/login' href='/login'>Login</NavItem>
-    return <div className='root-component-container'>
-      <Header>
-        <NavItem key='/#promo' href='/#promo'><h3>Willie & Allie</h3></NavItem>
-        <NavItem key='/#gigs' href='/#gigs'>Gigs</NavItem>
-        <NavItem key='/#music' href='/#music'>Music</NavItem>
-        <NavItem key='/#video' href='/#video'>Video</NavItem>
-        <NavItem key='/#contact' href='/#contact'>Contact</NavItem>
-        {navRight}
-      </Header>
-      {content}
-      <Footer>
-        <div className='copyright'>Willie & Allie © 2016</div>
-      </Footer>
-    </div>
-  }
-})
+  render() {
+    const content = this.props.content;
+    return (
+      <div className="root-component-container">
+        <Header>
+          <NavItem key="/#promo" href="/#promo">
+            <h3>Willie &amp; Allie</h3>
+          </NavItem>
+          <NavItem key="/#gigs" href="/#gigs">
+            Gigs
+          </NavItem>
+          <NavItem key="/#music" href="/#music">
+            Music
+          </NavItem>
+          <NavItem key="/#video" href="/#video">
+            Video
+          </NavItem>
+          <NavItem key="/#contact" href="/#contact">
+            Contact
+          </NavItem>
+        </Header>
+        {content}
+        <Footer>
+          <div className="copyright">Willie &amp; Allie © 2016</div>
+        </Footer>
+      </div>
+    );
+  },
+});
 
-module.exports = RootComponent
+module.exports = RootComponent;
